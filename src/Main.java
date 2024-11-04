ffdaad1fda036df104e72e54c17f3d04ba31f454
Sammie Zhang <101011086+sz6084@users.noreply.github.com>
2024-11-04 17:23:36 +0000

    Added basic menu items and some future guides

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 public class Main
 {
-
+        static String[] greet = {"Hello! Welcome to Ramen101!", "Salutations good customer! Welcome to Ramen101"};
     //CREATE INSTANCE VARIABLES HERE. Ensure they are static.
-    //May want to create an array of goodbye answers.
-    static String[] goodBye = {"bye"};
+    static String[] goodBye = {"bye", "see you later!", "good doing business with you"};
+    static String[] keywords = {"ramen", "bbq", "barbecue", "appetizers"};
+    static String[] ramenTypes = {"tonkotsu", "miso", "seafood", "black garlic", "chicken", "veggie", "veggy", "vegetable"};
+    static String[] bbqs = {"chicken katsu", "teriyaki chicken", "hawaiian bbq chicken", "hawaiian bbq beef", "hawaiian bbq short rib", "bbq chicken and beef", "bbq chicken and chicken katsu", "hawaiian bbq trio"};
+    static String[] appetizers = {"chicken kara-age", "grill whole squid", "gyoza", "agedashi tofu", "takoyaki", "prawn tempura", "vegetable tempura", "kaki fry", "geso fry", "spam musubi", "lumpia", "crab rangoons", "edamame", "spicy cucumber salad", "white rice"};
+    // bbq parsing
+    // parse input by lowering case
    
        public static void main(String[] args)
        {
         Scanner in = new Scanner (System.in);//Creates scanner object.
-        Scanner in2 = new Scanner (System.in);//Creates a second scanner object.
+        //Scanner in2 = new Scanner (System.in);//Creates a second scanner object.
         System.out.println("Bot says hi, user says:");
         String userResp = in.nextLine(); //in.nextLine() uses the scanner object to get the user's responnse as a String
-        System.out.println(userResp);
-        System.out.println("Bot says blah, expects an int now");
-        int numResp = in2.nextInt();//Gets the user's input as an integer. 
-
+        // System.out.println(userResp);
+        
 
         //You will need to make sure your chatbot continues looping until it sees certain keywords from the user such as "bye","goodbye"...
         // do you need a while loop or a for loop?
